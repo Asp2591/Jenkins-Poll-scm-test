c9424d81cc897b6cf30c680d00e0346362479f6c
Asp2591 <119059499+Asp2591@users.noreply.github.com>
2024-07-21 12:09:34 +0530

    Create app.py
    Python file added

---
+++ app.py
@@ -0,0 +1,14 @@
+## Age wise schemes
+
+age=int(input("Enter a your age :"))
+
+if age<19:
+  print("You are not adult ")
+elif (age>=19 and age<25):
+  print("You have scheme B to apply")
+elif (age>=25 and age<35):
+  print("You have scheme B to apply")
+elif (age>=35 and age<45):
+  print("You have scheme B to apply")
+else:
+  print("You can't apply for any scheme.")
