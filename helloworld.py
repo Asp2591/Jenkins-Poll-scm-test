bf9765b27fc2ae8d4f6f720aa2f6ba21196f6a04
Asp2591 <119059499+Asp2591@users.noreply.github.com>
2024-07-22 10:33:13 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return '<h1>Hello Guys !! Atharva Here</h2>'
 
 if __name__=="__main__":
-    app.run("0.0.0.0",port=7070)
+    app.run("0.0.0.0",port=8181)
