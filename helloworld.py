de430d99c617917ee71839d580a0b55a71d225db
Atharva S Patkar <119059499+Asp2591@users.noreply.github.com>
2024-09-17 11:55:16 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
     return '<h1>Hello Guyssss !! Atharva Here</h2>'
+@app.route('/about')
+def about():
+    return '<h1>Hello Guyssss !! Atharva Patkar Here</h2>'
+
 
 if __name__=="__main__":
     app.run("0.0.0.0",port=8181)
